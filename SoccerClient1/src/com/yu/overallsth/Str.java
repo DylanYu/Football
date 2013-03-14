3dd32ce9bcc50e9159979f6c3c5909911d3ab3bc
Yu Dongliang <yudl.nju@gmail.com>
2013-03-14 18:35:01 +0800

    delete useless part

--- SoccerClient1/src/com/yu/overallsth/Str.java
+++
@@ -1,7 +0,0 @@
-package com.yu.overallsth;
-
-public class Str {
-	public static final String COMMAND_UP = "command_up";
-	public static final String SIGNAL_UP = "signal_up";
-	public static final String NO_COMMAND = "no_Command";
-}
